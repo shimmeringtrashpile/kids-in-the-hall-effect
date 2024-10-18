823b98b5007103f0ea4b189cfd78290c8804b363
Rob Ray <47587146+shimmeringtrashpile@users.noreply.github.com>
2024-10-18 10:47:51 -0600

    Setup arduino for the first Hall effect sensor.

--- kids-in-the-hall-effect/kids-in-the-hall-effect.ino
+++ kids-in-the-hall-effect/kids-in-the-hall-effect.ino
@@ -1,5 +1,18 @@
+const int hallPin = 2; // Hall effect sensor pin
+const int ledPin = 13; // LED pin
+int hallState = 0; // Hall effect sensor state
+
 void setup() {
   // put your setup code here, to run once:
+  // set the hallPin as an input
+  pinMode(hallPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  
+  // turn on the internal pull-up resistor
+  digitalWrite(hallPin, HIGH);
+
+  // initialize the serial communication
+  Serial.begin(9600);
 
 }
 
