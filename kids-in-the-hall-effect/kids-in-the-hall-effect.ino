265b8df5584304b8c3a334f993b07e10f860008e
Rob Ray <rob@shimmeringtrashpile.com>
2024-10-19 21:18:02 -0600

    First latching hall effect sensor

--- kids-in-the-hall-effect/kids-in-the-hall-effect.ino
+++ kids-in-the-hall-effect/kids-in-the-hall-effect.ino
@@ -1,17 +1,17 @@
 #include <Arduino.h>
 
 const int latchPin = 2; // US1881 Non-Latching Hall effect sensor pin
-const int hallPin = 3; // AH1815 Latching Hall effect sensor pin
+// const int hallPin = 3; // AH1815 Latching Hall effect sensor pin
 const int ledPin = 13; // Built-in LED pin
 int latchState = LOW; // Hall effect sensor state
-int hallState = LOW; // Hall effect sensor state
+// int hallState = LOW; // Hall effect sensor state
 
 
 void setup() {
   // put your setup code here, to run once:
   // set the hallPin as an input
   pinMode(latchPin, INPUT);
-  pinMode(hallPin, INPUT);
+  // pinMode(hallPin, INPUT);
   pinMode(ledPin, OUTPUT);
   
   // turn on the internal pull-up resistors
@@ -26,19 +26,19 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   latchState = digitalRead(latchPin);
-  hallState = digitalRead(hallPin);
+  // hallState = digitalRead(hallPin);
 
   if (latchState == HIGH) {
     // turn on the LED
     digitalWrite(ledPin, HIGH);
-    Serial.println("Magnet on Latching!");
-  } else if (hallState == LOW) { // The US1881 is open drain. LOW when magnet passes.
-    // turn on the LED
-    digitalWrite(ledPin, HIGH);
-    Serial.println("Magnet on Hall!");
+    Serial.println("Latching!");
+  // } else if (hallState == LOW) { // The US1881 is open drain. LOW when magnet passes.
+  //   // turn on the LED
+  //   digitalWrite(ledPin, HIGH);
+  //   Serial.println("Magnet on Hall!");
   } else {
     // turn off the LED
     digitalWrite(ledPin, LOW);
-    Serial.println("No!");
+    Serial.println("No Latch!");
   }
 }
